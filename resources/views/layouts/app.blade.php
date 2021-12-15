a56bb5903dcd6f3b9207f7f00926109d2afed0c1
Benjamín Olvera Rosales <benjaminolverarosales@gmail.com>
2021-12-14 19:09:31 -0600

    Layout y rutas web

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <title>{{ config('app.name') }} | @yield('title')</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+    <script src="{{ asset('js/app.js') }}"></script>
+    @livewireStyles
+</head>
+
+<body>
+    <div id="content">
+        @yield('content')
+    </div>
+    @livewireScripts
+    @yield('scripts')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers as Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::name('app.')->group(function () {
+    Route::get('/', [Controller\AppController::class, 'inicio'])->name('inicio');
 });
