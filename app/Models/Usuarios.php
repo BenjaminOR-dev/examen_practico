f888666e329d781277ac2c3659ac14bd0e9267bd
Benjamín Olvera Rosales <benjaminolverarosales@gmail.com>
2021-12-14 21:19:27 -0600

    Migrations

--- app/Models/Usuarios.php
+++ app/Models/Usuarios.php
@@ -12,13 +12,20 @@ class Usuarios extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+
+    protected $table = 'usuarios';
+    protected $primaryKey = 'id';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'id_rol',
+        'nombre',
+        'apellido_paterno',
+        'apellido_materno',
         'email',
         'password',
     ];
@@ -41,4 +48,30 @@ class Usuarios extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    /**
+     * Accesores
+     */
+    protected $appends = [
+        'nombre_completo'
+    ];
+
+    public function getNombreCompletoAttribute()
+    {
+        return "{$this->nombre} {$this->apellido_paterno} {$this->apellido_materno}";
+    }
+
+    /**
+     * Relaciones
+     */
+
+    protected $with = [
+        'rol'
+    ];
+
+    public function rol()
+    {
+        return $this->hasOne(Roles::class, 'id', 'id_rol');
+    }
 }
